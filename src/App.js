9695d7d7c35f2f5af5c1ad2c215a15a78d36b9ec
Amelia Caffrey <amelia.caffrey@zuhlke.com>
2022-01-14 19:12:25 +0000

    Welcome to dungasteez compiling, video 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hi
-        </p>
-
-      </header>
-    </div>
-  );
+    <>
+      <h1>Welcome to DUNGA-STEEZ</h1>
+    </>
+  )
 }
 
 export default App;
